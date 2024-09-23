e92c4314d6032dc77ed505015befea5f1d6031ba
Katlego R. Phele <113664624+katlegorphele@users.noreply.github.com>
2024-09-24 00:53:59 +0200

    Update useWeb3.ts

--- packages/react-app/contexts/useWeb3.ts
+++ packages/react-app/contexts/useWeb3.ts
@@ -2,21 +2,79 @@ import { BrowserProvider, Contract, parseUnits } from "ethers";
 import { useState } from "react";
 import StableTokenABI from "./cusd-abi.json";
 import ValoraNFTABI from "./valora-nft.json";
+import CollateralManagerABI from "./abis/CollateralManager.json";
+import LoanManagerABI from "./abis/LoanManager.json";
+import { stableTokenABI } from "@celo/abis";
+
+
 
 export const useWeb3 = () => {
   const [address, setAddress] = useState<string | null>(null);
   const cUSDTokenAddress = "0x765de816845861e75a25fca122bb6898b8b1282a";
   const VALORA_NFT_CONTRACT = "0xDEd283f8Cc841a53BC2A85AD106b2654E650Cc7f";
+  const COLLATERAL_MANAGER_CONTRACT = "0xf3978E55d052124178dBd34729dab91cD39dc23D";
+  const Loan_contract_address = "0x20dc701Ef6265C7Fb6a81680aB662205Ce5d10D8";
+  const cUER_CONTRACT_ADDRESS = "0x10c892a6ec43a53e45d0b916b4b7d383b1b78c0f";
+  const cusd_CONTRACT_ADDRESS = "0x874069fa1eb16d44d622f2e0ca25eea172369bc1";
+
 
   const getUserAddress = async () => {
     if (typeof window !== "undefined" && window.ethereum) {
       const provider = new BrowserProvider(window.ethereum);
       await provider.send("eth_requestAccounts", []);
       const signer = await provider.getSigner();
-      const addressFromWallet = await signer.getAddress();
+      const addressFromWallet = await signer.getAddress(); 
       setAddress(addressFromWallet);
     }
   };
+
+
+
+  const depositCeloCollateral = async (amount: string) => {
+    if (typeof window !== "undefined" && window.ethereum) {
+      const provider = new BrowserProvider(window.ethereum);
+      await provider.send("eth_requestAccounts", []);
+      const signer = await provider.getSigner();
+      const collateralManagerContract = new Contract(
+        COLLATERAL_MANAGER_CONTRACT,
+        CollateralManagerABI.abi,
+        signer
+      );
+      // const amountInWei = parseUnits(amount, 18);
+      const tx = await collateralManagerContract.depositCeloCollateral({
+        value: parseUnits(amount, 18), // converting amount to wei
+      });
+      await tx.wait();
+      return tx;
+    }
+    }
+
+    const depositUsdtCollateral = async (amount: string) => {
+      if (typeof window !== "undefined" && window.ethereum) {
+        const provider = new BrowserProvider(window.ethereum);
+        await provider.send("eth_requestAccounts", []);
+        const signer = await provider.getSigner();
+        const collateralManagerContract = new Contract(
+          COLLATERAL_MANAGER_CONTRACT,
+          CollateralManagerABI.abi,
+          signer
+        );
+        // const amountInWei = parseUnits(amount, 18);
+        const cUSDTokenContract = new Contract(
+          cUER_CONTRACT_ADDRESS,
+          StableTokenABI.abi,
+          signer
+        );
+
+        const approveTx = await cUSDTokenContract.approve(COLLATERAL_MANAGER_CONTRACT, parseUnits(amount, 18));
+
+        await approveTx.wait();
+        const tx = await collateralManagerContract.depositUsdtCollateral(parseUnits(amount, 18));
+        await tx.wait();
+        return tx;
+      }
+      }
+
 
   const sendCUSD = async (to: string, amount: string) => {
     const provider = new BrowserProvider(window.ethereum);
@@ -31,6 +89,8 @@ export const useWeb3 = () => {
     await tx.wait();
     return tx;
   };
+
+
 
   const mintValoraNFT = async () => {
     const provider = new BrowserProvider(window.ethereum);
@@ -84,5 +144,7 @@ export const useWeb3 = () => {
     mintValoraNFT,
     getNFTs,
     signTransaction,
+    depositCeloCollateral,
+    depositUsdtCollateral,
   };
 };
